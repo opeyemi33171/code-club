602f518aaa74a7eef0d6f1e25b97b6ab3daa04fa
joshua.bellew <joshua.bellew@coop.co.uk>
2020-07-28 13:59:20 +0100

    Checkout code club continue, next week TODO: Refactor

--- 2020-07-21/checkout.test.js
+++ 2020-07-21/checkout.test.js
@@ -4,22 +4,51 @@ const fetch = require("node-fetch");
 const http = require("http");
 
 describe("Checkout kata", () => {
-  xit("adds the value of the scaned item", () => {
-    const total = scan();
-    assert(total).expect("2.89");
-  });
   const port = 7070;
-  http.createServer((req, res) => {
-    // res = scan()
-    res.end()
-  }).listen(port);
+  let server;
+  let total;
 
-  it("returns ok response when item scanned", async () => {
-    
+  beforeEach(() => {
+    total = 0;
 
-    const response = await fetch(`http://localhost:${port}/scan/item1`);
+    server = http.createServer((req, res) => {
+      if (req.method == "POST") total++;
+      res.statusCode = req.method === "GET" ? 200 : 204;
 
-    assert.equal(response.status,200);
+      res.end(total.toString());
+    });
+    server.listen(port);
   });
-  
+
+  afterEach(() => {
+    server.close();
+  });
+
+  it("returns 204 ok response when item scanned", async () => {
+    const response = await fetch(`http://localhost:${port}/scan/item1`, {
+      method: "POST",
+    });
+
+    assert.equal(response.status, 204);
+  });
+
+  it("returns 200 ok response when total is requested", async () => {
+    const response = await fetch(`http://localhost:${port}/total`, {
+      method: "GET",
+    });
+
+    assert.equal(response.status, 200);
+    assert.equal(await response.text(), "0");
+  });
+
+  it("returns non zero total after item is scanned", async () => {
+    await fetch(`http://localhost:${port}/scan/item1`, {
+      method: "POST",
+    });
+    const response = await fetch(`http://localhost:${port}/total`, {
+      method: "GET",
+    });
+    assert.equal(response.status, 200);
+    assert.notEqual(await response.text(), "0");
+  });
 });
