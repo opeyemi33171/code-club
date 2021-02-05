59c332039deff7d71fca6aec7d9d73fa47ee2c69
James Jeffries <james.jeffries@coopdigital.co.uk>
2021-02-05 16:57:27 +0000

    another week's code

--- 2020-08-28/potter-kata.test.js
+++ 2020-08-28/potter-kata.test.js
@@ -1,26 +1,61 @@
 const assert = require("assert");
-const x = (items) => ({
-  total: items.length * 8,
-  items,
-});
+
+const bookPrice = 8;
+
+const applyDiscountedPrice = (numberUniqueItems, discountedPrice) =>
+  numberUniqueItems * bookPrice * discountedPrice;
+
+const bananas = (numberUniqueItems, numberOfItems) => {
+  if (numberUniqueItems == 3) {
+    return applyDiscountedPrice(numberUniqueItems, 0.9);
+  }
+  if (numberUniqueItems == 2 && numberUniqueItems == numberOfItems) {
+    return applyDiscountedPrice(numberUniqueItems, 0.95);
+  }
+  return applyDiscountedPrice(numberOfItems, 1);
+};
+
+const calculateBasketTotal = (items) => {
+  const numberOfItems = items.length;
+  const itemSet = new Set(items.map((book) => book.title));
+  const numberUniqueItems = itemSet.size;
+
+  return bananas(numberUniqueItems, numberOfItems);
+};
+
+const createItem = (title) => ({ title });
+
 describe("potter books kata", () => {
   it("should cost 0 euros for an empty basket", () => {
-    const basket = x();
-    expect(basket.total).toEqual(0);
-    expect(basket.items.length).toEqual(0);
+    const total = calculateBasketTotal([]);
+    expect(total).toEqual(0);
   });
 
-  // it("should cost 8 euros for a basket containing one book", () => {
-  //   const basket = {
-  //     total: 8,
-  //     items: [""],
-  //   };
-  //   expect(basket.total).toEqual(8);
-  //   expect(basket.items.length).toEqual(1);
-  // });
+  it("should cost 8 euros for a basket containing one book", () => {
+    const total = calculateBasketTotal([createItem("book one")]);
+    expect(total).toEqual(8);
+  });
 
-  // it("should cost 16 euros for two of the same book", () => {
-
-  //   expect([book.cost, book.cost]).toEqual(16);
-  // });
+  it("should cost 15.20 euros for two different books", () => {
+    const total = calculateBasketTotal([
+      createItem("Book one"),
+      createItem("Book two"),
+    ]);
+    expect(total).toEqual(15.2);
+  });
+  it("should cost 16 euros for two of the same books", () => {
+    const total = calculateBasketTotal([
+      createItem("Book one"),
+      createItem("Book one"),
+    ]);
+    expect(total).toEqual(16);
+  });
+  it("should cost 21.60 euros for three different books", () => {
+    const total = calculateBasketTotal([
+      createItem("Book one"),
+      createItem("Book two"),
+      createItem("Book three"),
+    ]);
+    expect(total).toEqual(21.6);
+  });
 });
--- 2021-01-29/calculator.test.js
+++ 2021-01-29/calculator.test.js
@@ -1,8 +1,19 @@
 function add(numbers) {
-  if (numbers.length > 0) {
-    return parseInt(numbers);
+  if (numbers.length === 0) {
+    return 0;
   }
-  return numbers.length;
+  if (numbers.length >= 3) {
+    let numbersArray = numbers.split(",");
+    if (numbersArray.length === 3) {
+      return (
+        parseInt(numbersArray[0]) +
+        parseInt(numbersArray[1]) +
+        parseInt(numbersArray[2])
+      );
+    }
+    return parseInt(numbersArray[0]) + parseInt(numbersArray[1]);
+  }
+  return parseInt(numbers);
 }
 
 describe("calculator tests", () => {
@@ -16,5 +27,18 @@ describe("calculator tests", () => {
 
   it("should return two for a string of '2'", () => {
     expect(add("2")).toBe(2);
+  });
+
+  it("should return 2 for a string of '1,1'", () => {
+    expect(add("1,1")).toBe(2);
+  });
+  it("should return three for a string of '1,2", () => {
+    expect(add("1,2")).toBe(3);
+  });
+  it("should return 11 for a string of '1,10'", () => {
+    expect(add("1,10")).toBe(11);
+  });
+  it("should return 3 for a string of '1,1,1'", () => {
+    expect(add("1,1,1")).toBe(3);
   });
 });
